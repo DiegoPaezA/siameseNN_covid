d27b5f78db686f3ef352910ab4b4058ee43bab03
DiegoPaez <diripar8@gmail.com>
2023-04-28 13:28:03 -0300

    training bug fixed - vgg16test

--- utils/siamese_network.py
+++ utils/siamese_network.py
@@ -1,7 +1,8 @@
 import numpy as np
 import torch
 import torch.nn.functional as F
-import torchvision.models as models
+from torchvision.models import vgg16, VGG16_Weights
+
 import torch.nn as nn
 
 
@@ -12,9 +13,6 @@ class SiameseNetwork(nn.Module):
     
     Args:
         nn.Module: Pytorch neural network module.
-    
-        
-    
     """
 
     def __init__(self):
@@ -22,9 +20,11 @@ class SiameseNetwork(nn.Module):
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
         # Setting up the Sequential of CNN Layers
-        self.Vgg11 = models.vgg11(weights=True)
-        num_ftrs = self.Vgg11.classifier[6].in_features
-        self.Vgg11.classifier[6] = nn.Linear(num_ftrs, 4)
+        weights = VGG16_Weights.DEFAULT
+        self.Vgg16 = vgg16(weights=weights)
+        #num_ftrs = self.Vgg16.classifier[6].in_features
+        #self.Vgg16.classifier[6] = nn.Linear(num_ftrs, 4)
+        self.Vgg16.classifier[6] = nn.Linear(in_features = self.Vgg16.classifier[6].in_features, out_features=4)
         
         self.counter = []
         self.acc_train =  []
@@ -44,7 +44,7 @@ class SiameseNetwork(nn.Module):
             output (torch.Tensor): Output tensor with shape (batch_size, embedding_size)
         
         """
-        output =self.Vgg11(x)
+        output =self.Vgg16(x)
         return output
 
     def forward(self, input1, input2):
@@ -82,12 +82,11 @@ class SiameseNetwork(nn.Module):
         # Iterate throught the epochs
         train_dataloader = dataloaders['train']
         val_dataloader = dataloaders['val']
-        iteration_number = 0
-        
+                
         for epoch in range(num_epochs):
             print('Epoch {}/{}'.format(epoch+1, num_epochs))
             print('-'*30)
-            y=0
+            
             for phase in ['train','val']:
                 if phase == 'train':
                     model.train()
@@ -99,11 +98,12 @@ class SiameseNetwork(nn.Module):
                 running_loss = RunningMetric()  #perdida tasa de error de la error
                 running_acc = RunningMetric()   #precision
                               
-                label_batches = torch.ones(64).to(self.device)               
+                label_batches = torch.ones(64).to(self.device)
+                               
                 for l, (img0, img1, label) in enumerate(datos,0):
                     # Take a batch of images and labels and send them to the device
-                    img0, img1, label = img0.to(self.device), img1.to(self.device), label.to(self.device)
-                    
+                    img0, img1, label = img0.to(self.device),img1.to(self.device), label.to(self.device)
+                    print(f"iteracion {l+1} de {len(datos)}")
                     optimizer.zero_grad()     #llevar a cero..reiniciar
                     with torch.set_grad_enabled(phase=='train'):
                             
@@ -115,12 +115,12 @@ class SiameseNetwork(nn.Module):
                             _,preds_cnn1 = torch.max(output1,1)
                             _,preds_cnn2 = torch.max(output2,1)
                             
-                            label = torch.squeeze(label)
+                            label_p = torch.squeeze(label)
                             preds_siamese=torch.ones(64).to(self.device) # 64 is the batch size
        
                             # Compare the two outputs and determine if they are images of the same class
                             for idx, val in enumerate(preds_cnn1):
-                                label_batches[idx]=label[idx]
+                                label_batches[idx]=label_p[idx]
                                 if preds_cnn2[idx]==preds_cnn1[idx]:
                                     preds_siamese[idx]=0
                                 if preds_cnn2[idx]!=preds_cnn1[idx]:
@@ -137,10 +137,11 @@ class SiameseNetwork(nn.Module):
                         
                     batch_size=img0.size()[0]
                     
+                    
                     running_loss.update(loss_contrastive.item()*batch_size,batch_size)
                     running_acc.update(torch.sum(preds_siamese==label_batches).float(),batch_size)
                         
-                    if l % len(datos) == 0:
+                    if (l+1) / len(datos) == 1:
                         if(phase=='train'):
                             print(f"Epoch: {epoch+1}/{num_epochs}, updated train metrics, acc: {running_acc()}, loss: {round(running_loss(),3)}")
                             self.counter.append(epoch)
